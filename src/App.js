67809ad223512733aa4fa892473a98627e678ca7
Arjun Devendra <arjundevendra34@gmail.com>
2024-04-29 21:08:47 +0530

    responsive complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import MovieComponenet from "./components/MovieComponent";
 
@@ -7,28 +7,50 @@ import { FaFilter } from "react-icons/fa";
 
 import data from "./data/data.json";
 function App() {
-  const [movie, setMovie] = useState(data);
-  const slicedMovie = movie.slice(0, 8);
-  console.log(movie);
+  const [movies, setMovies] = useState(data);
+  const [page, setPage] = useState(1);
+
+  const handlePagination = (pgs) => {
+    setPage(pgs);
+  };
   return (
     <>
       <div className="header">
-        <h2>Movie List</h2>
-        <div className="options">
-          <button className="btn">
-            <TiSortAlphabetically />
-            sort
-          </button>
-          <button className="btn">
-            <FaFilter />
-            filter
-          </button>
+        <div className="nav-bar">
+          <h2>Movie List</h2>
+          <div className="options">
+            <button className="btn">
+              <TiSortAlphabetically />
+              sort
+            </button>
+            <button className="btn">
+              <FaFilter />
+              filter
+            </button>
+          </div>
         </div>
+      </div>
+      <div className="pagination-bar">
+        {movies.length > 0 && (
+          <div className="pagination">
+            <span className="pg-btn">&larr;</span>
+            {[...Array(movies.length / 10)].map((i, pg) => (
+              <span
+                key={i}
+                className={page === pg + 1 ? "pg-btn-selected" : "pg-btn"}
+                onClick={() => handlePagination(pg + 1)}
+              >
+                {pg + 1}
+              </span>
+            ))}
+            <span className="pg-btn">&rarr;</span>
+          </div>
+        )}
       </div>
       <div className="main">
         <div className="inner-box">
           <div className="card-container">
-            {slicedMovie.map((movie) => (
+            {movies.slice(page * 10 - 10, page * 10).map((movie) => (
               <MovieComponenet data={movie} />
             ))}
           </div>
--- src/components/MovieComponent.jsx
+++ src/components/MovieComponent.jsx
@@ -6,54 +6,33 @@ const MovieComponenet = ({ data }) => {
   const [countries, setCountries] = useState(data.moviecountries);
   const [genres, setGenres] = useState(data.moviegenres);
 
-  //For Languages
-  const slicedLanguages = languages.slice(0, 3);
-  const hasMoreLang = languages.length > 3;
-
-  //For Countries
-  const slicedCountries = countries.slice(0, 2);
-  const hasMoreCount = countries.length > 2;
-
-  //For Genres
-  const slicedGenre = genres.slice(0, 2);
-  const hasMoreGen = genres.length > 2;
-
   return (
     <div className="parent-card">
       <img src={data.moviemainphotos} alt="movie_img" className="movie_img" />
       <div className="movie_information">
         <h3 className="title">{data.movietitle}</h3>
-        <p className="">
-          {slicedLanguages.map((l, idx) => (
+        <p className="movie-details">
+          {languages.map((l, idx) => (
             <span key={idx} className="languages">
               {l}
             </span>
           ))}
-          {hasMoreLang && (
-            <span style={{ fontWeight: "600", marginLeft: "5px" }}>+</span>
-          )}
         </p>
-        <p className="">
-          {slicedCountries
-            ? slicedCountries.map((l, idx) => (
+        <p className="movie-details">
+          {countries
+            ? countries.map((l, idx) => (
                 <span key={idx} className="languages">
                   {l}
                 </span>
               ))
             : "No Countries"}
-          {hasMoreCount && (
-            <span style={{ fontWeight: "600", marginLeft: "5px" }}>+</span>
-          )}
         </p>
-        <p className="">
-          {slicedGenre.map((l, idx) => (
+        <p className="movie-details">
+          {genres.map((l, idx) => (
             <span key={idx} className="languages">
               {l}
             </span>
           ))}
-          {hasMoreGen && (
-            <span style={{ fontWeight: "600", marginLeft: "5px" }}>+</span>
-          )}
         </p>
       </div>
     </div>
