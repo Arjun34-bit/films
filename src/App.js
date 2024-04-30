7614fa363afde7fcadfcb89c7ab5496eb0fe1d64
Arjun Devendra <arjundevendra34@gmail.com>
2024-05-01 01:41:23 +0530

    filters frontend

--- src/App.js
+++ src/App.js
@@ -3,13 +3,79 @@ import "./App.css";
 import MovieComponenet from "./components/MovieComponent";
 
 import { TiSortAlphabetically } from "react-icons/ti";
+import { GiFilmSpool } from "react-icons/gi";
 import { FaFilter } from "react-icons/fa";
 
 import data from "./data/data.json";
+import Portal from "./Portal/Portal.jsx";
 function App() {
   const [movies, setMovies] = useState(data);
   const [page, setPage] = useState(1);
 
+  const [isOpen, setIsOpen] = useState(false);
+
+  const allLanguages = movies.reduce((acc, movie) => {
+    return acc.concat(movie.movielanguages);
+  }, []);
+
+  const allGenres = movies.reduce((acc, movie) => {
+    return acc.concat(movie.moviegenres);
+  }, []);
+
+  const allCountries = movies.reduce((acc, movie) => {
+    return acc.concat(movie.moviecountries);
+  }, []);
+
+  // Create sets to remove duplicates
+  const uniqueLanguages = [...new Set(allLanguages)];
+  const uniqueGenres = [...new Set(allGenres)];
+  const uniqueCountries = [...new Set(allCountries)];
+
+  const [selectedLanguages, setSelectedLanguages] = useState([]);
+  const [selectedGenres, setSelectedGenres] = useState([]);
+  const [selectedCountries, setSelectedCountries] = useState([]);
+
+  const handleCheckboxChange = (e, type) => {
+    const value = e.target.name;
+    switch (type) {
+      case "language":
+        setSelectedLanguages((prev) =>
+          e.target.checked
+            ? [...prev, value]
+            : prev.filter((lang) => lang !== value)
+        );
+        break;
+      case "genre":
+        setSelectedGenres((prev) =>
+          e.target.checked
+            ? [...prev, value]
+            : prev.filter((genre) => genre !== value)
+        );
+        break;
+      case "country":
+        setSelectedCountries((prev) =>
+          e.target.checked
+            ? [...prev, value]
+            : prev.filter((country) => country !== value)
+        );
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleClearAll = () => {
+    setSelectedLanguages([]);
+    setSelectedGenres([]);
+    setSelectedCountries([]);
+  };
+
+  const handleApplyFilters = () => {
+    // Apply filters based on selectedLanguages, selectedGenres, selectedCountries
+    console.log("Languages:", selectedLanguages);
+    console.log("Genres:", selectedGenres);
+    console.log("Countries:", selectedCountries);
+  };
   const handlePagination = (pgs) => {
     setPage(pgs);
   };
@@ -20,30 +86,83 @@ function App() {
           <h2>Movie List</h2>
           <div className="options">
             <button className="btn">
-              <TiSortAlphabetically />
-              sort
-            </button>
-            <button className="btn">
-              <FaFilter />
-              filter
+              <GiFilmSpool /> Arjun
             </button>
           </div>
         </div>
       </div>
       <div className="pagination-bar">
+        <div className="filter-btn">
+          <button onClick={() => setIsOpen(true)} className="btn">
+            <FaFilter />
+            filter
+            <Portal open={isOpen} onClose={() => setIsOpen(false)}>
+              <div className="checkBox-container">
+                <div className="lang">
+                  <h3 className="head">Languages</h3>
+                  {uniqueLanguages.map((item) => (
+                    <div key={item.id} className="lang-checkbox">
+                      <label>
+                        <input
+                          type="checkbox"
+                          name={item}
+                          checked={selectedLanguages.includes(item)}
+                          onChange={(e) => handleCheckboxChange(e, "language")}
+                        />
+                        {item}
+                      </label>
+                    </div>
+                  ))}
+                </div>
+                <div className="lang">
+                  <h3 className="head">Genre</h3>
+                  {uniqueGenres.map((item) => (
+                    <div key={item.id} className="lang-checkbox">
+                      <label>
+                        <input
+                          type="checkbox"
+                          name={item}
+                          checked={selectedGenres.includes(item)}
+                          onChange={(e) => handleCheckboxChange(e, "genre")}
+                        />
+                        {item}
+                      </label>
+                    </div>
+                  ))}
+                </div>
+                <div className="lang">
+                  <h3 className="head">Countries</h3>
+                  {uniqueCountries.map((item) => (
+                    <div key={item.id} className="lang-checkbox">
+                      <label>
+                        <input
+                          type="checkbox"
+                          name={item}
+                          checked={selectedCountries.includes(item)}
+                          onChange={(e) => handleCheckboxChange(e, "country")}
+                        />
+                        {item}
+                      </label>
+                    </div>
+                  ))}
+                </div>
+              </div>
+              <div className="bottom-btn">
+                <button onClick={handleClearAll}>Clear All</button>
+                <button onClick={handleApplyFilters}>Apply</button>
+              </div>
+            </Portal>
+          </button>
+        </div>
         {movies.length > 0 && (
           <div className="pagination">
-            <span className="pg-btn">&larr;</span>
             {[...Array(movies.length / 10)].map((i, pg) => (
               <span
                 key={i}
                 className={page === pg + 1 ? "pg-btn-selected" : "pg-btn"}
                 onClick={() => handlePagination(pg + 1)}
-              >
-                {pg + 1}
-              </span>
+              ></span>
             ))}
-            <span className="pg-btn">&rarr;</span>
           </div>
         )}
       </div>
---
+++ src/Portal/Portal.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const modalStyles = {
+  position: `fixed`,
+  top: `50%`,
+  left: `50%`,
+  transform: `translate(-50%,-50%)`,
+  backgroundColor: `#FFF`,
+  padding: `50px`,
+  zIndex: 1000,
+  height: "65vh",
+  width: "50vw",
+};
+
+const overlay_style = {
+  position: `fixed`,
+  top: 0,
+  left: 0,
+  bottom: 0,
+  right: 0,
+  backgroundColor: `rgba(0, 0, 0, 0.5)`,
+  zIndex: 1000,
+};
+
+export default function Portal({ open, children, onClose }) {
+  if (!open) {
+    return null;
+  }
+  return ReactDOM.createPortal(
+    <>
+      <div style={overlay_style}>
+        <div style={modalStyles}>
+          <button onClick={onClose}>Close</button>
+          {children}
+        </div>
+      </div>
+    </>,
+    document.getElementById("portal")
+  );
+}
